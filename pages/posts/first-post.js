4c87226b2c14ff58cf30a8b54c411e050afd7571
Anton Metlitsky <ticket1201@gmail.com>
2023-01-03 03:05:57 +0400

    feat: impl first-post component

---
+++ pages/posts/first-post.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+import Layout from '../../components/layout';
+
+const FirstPost = () => {
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
+        </Layout>
+    );
+};
+
+export default FirstPost;
