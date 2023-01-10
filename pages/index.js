25fe8f0fe06e0602f1cccededb1d28649c0335eb
Anton Metlitsky <ticket1201@gmail.com>
2023-01-11 01:16:22 +0400

    feat: rework component with human-readable date, add link to dynamic route pages

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Layout, {siteTitle} from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import { getSortedPostsData } from '../lib/posts';
+import Date from '../components/date';
 
 export default function Home({allPostsData}) {
     return (
@@ -15,11 +16,11 @@ export default function Home({allPostsData}) {
                 <ul className={utilStyles.list}>
                     {allPostsData.map(({ id, date, title }) => (
                         <li className={utilStyles.listItem} key={id}>
-                            {title}
+                            <Link href={`/posts/${id}`} className={'text-blue/50'}>{title}</Link>
                             <br />
-                            {id}
-                            <br />
-                            {date}
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>
                         </li>
                     ))}
                 </ul>
